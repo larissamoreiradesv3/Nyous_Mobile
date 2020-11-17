f19fa2aabdec8f3b01bbb7167d62f3d81f4218a1
Larissa Moreira <larissamoreira.s2003@gmail.com>
2020-11-17 14:44:11 -0200

    Imports da parte App.js feitos

--- nyous_mobile/App.js
+++ nyous_mobile/App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//Navigations 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigation } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+const Stack  = createStackNavigator();
+
+//Páginas
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
