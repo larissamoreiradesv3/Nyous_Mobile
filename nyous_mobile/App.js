a0f3a9352bd035c3e7fc03e33b843670ec1ac450
Larissa Moreira <larissamoreira.s2003@gmail.com>
2020-11-17 16:35:20 -0200

    Alterações no designer

--- nyous_mobile/App.js
+++ nyous_mobile/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 //Navigations 
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigation } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
@@ -13,11 +13,20 @@ const Stack  = createStackNavigator();
 import Login from './pages/Login';
 import Home from './pages/Home';
 
+const Autenticado = () => {
+  return(
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={Home}/>
+    </Drawer.Navigator>
+  )
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown : false}}>
       <Stack.Screen name="Login" component={Login}/>
+      <Stack.Screen name="Autenticado" component={Autenticado}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- nyous_mobile/pages/Login/index.js
+++ nyous_mobile/pages/Login/index.js
@@ -1,2 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+const Login = () => {
+
+    const [email, setEmail] = useState( '' );
+    const [senha, setSenha] = useState( '' );
+
+
+    const Logar = () => {
+
+    }
+
+    return(
+        <View styles={styles.container}>
+            <TextInput
+            style={styles.input}
+            onChangeText={text => setEmail(text)}
+            value={email}
+            placeholder="Digite seu e-mail:"
+            />
+
+            <TextInput
+            style={styles.input}
+            onChangeText={text => setSenha(text)}
+            value={senha}
+            placeholder="Digite sua senha:"
+            secureTextEntry={true}
+            />
+             <TouchableOpacity
+            style={styles.button}
+            onPress={onPress}
+            >
+            <Text>Press Here</Text>
+             </TouchableOpacity>
+        </View>
+    )        
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input : { 
+        width       : "90%" ,
+        height      : 40, 
+        borderColor : 'gray',
+        borderWidth : 1,
+        marginTop   : 20,
+        padding     : 5,
+        borderRadius: 6
+    }
+  });
+
+export default Login;
